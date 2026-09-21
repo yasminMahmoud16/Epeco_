7c500632f4595298c18442ca88d2825defa988af
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2026-09-21 14:48:48 +0300

    Fixing the URL for github

--- src/Hooks/useUrl.js
+++ src/Hooks/useUrl.js
@@ -1,6 +1,6 @@
 
 export default function useUrl() {
-    const baseUrl = "https://yasminMahmoud16.github.io/Epeco/";
+    const baseUrl = "https://yasminMahmoud16.github.io/Epeco_/";
     // const baseUrl = "http://localhost:5173/";
     return { baseUrl }
 }
